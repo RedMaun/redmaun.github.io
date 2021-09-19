97db2de3a4a56b1e91944bf33bbf6e06ecdb1d7a
RedMaun <deb.il.dpkg@gmail.com>
2021-09-19 22:40:22 +0700

    keyboard

---
+++ keyboard/main.js
@@ -0,0 +1,5 @@
+$( document ).ready(function() {
+    document.getElementById('block2').style.opacity = "1";
+    document.getElementById('right').style.opacity = "1";
+    document.getElementById('left').style.opacity = "1";
+});
